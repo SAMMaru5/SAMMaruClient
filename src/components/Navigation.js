c5b22eba38c9522fabf2dd7ff00d9507ccca8c3e
Lucas0105 <dnjswo105@gmail.com>
2022-02-05 19:28:20 +0900

    add mainPage components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,9 +1,66 @@
-
+import './Navigation.scss'
 
 function Navigation() {
     return (
         <div className="Navigation">
-            <h1>Navigation</h1>
+            <div className='userStatus'>
+                <p className='attendance'>
+                    접속자(0명)
+                </p>
+
+                <p className='user'>
+                    회원가입/로그인
+                </p>
+            </div>
+
+            <nav>
+                <img src="img/logo.png" alt="로고 이미지"/>
+
+                <div className="dropdown">
+                    <button className="dropbtn">샘마루</button>
+                    <div className="dropdown-content">
+                    <a href="#!">Link 1</a>
+                    <a href="#!">Link 2</a>
+                    <a href="#!">Link 3</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn">공지사항</button>
+                    <div className="dropdown-content">
+                    <a href="#!">Link 1</a>
+                    <a href="#!">Link 2</a>
+                    <a href="#!">Link 3</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn">자유게시판</button>
+                    <div className="dropdown-content">
+                    <a href="#!">특강 자료</a>
+                    <a href="#!">활동보고서</a>
+                    <a href="#!">소규모프젝</a>
+                    <a href="#!">족보</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn">사진첩</button>
+                    <div className="dropdown-content">
+                    <a href="#!">Link 1</a>
+                    <a href="#!">Link 2</a>
+                    <a href="#!">Link 3</a>
+                    </div>
+                </div>
+                <div className="dropdown">
+                    <button className="dropbtn">충북대학교</button>
+                    <div className="dropdown-content">
+                    <a href="#!">충북대학교</a>
+                    <a href="#!">전자정보대학</a>
+                    <a href="#!">소프트웨어학과</a>
+                    <a href="#!">SW중심사업단</a>
+                    </div>
+                </div>
+            </nav>
+            
+
         </div>
 
     )
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -5,23 +5,26 @@ import MainScheduleDetaile from './MainScheduleDetaile'
 import Footer from '../../components/Footer'
 import Navigation from '../../components/Navigation'
 
+import './MainPage.scss'
 import "./clubIntroduction.scss";
 import "./clubActivities.scss";
 
 function MainPage() {
     return(
         <div className='MainPage'>
+            <div className='container'>
             <Navigation />
             <MainBoard />
-            <MainSchedule />
             <MainActivity />
+            <MainSchedule />
             { clubIntroduction() }
-        {clubActivity_01()}
-        {clubActivity_02()}
+            {clubActivity_01()}
+            {clubActivity_02()}
             <MainScheduleDetaile />
 
             {/* 동아리 수상실적 */}
             <Footer />
+            </div>
         </div>
     )
 }
@@ -73,7 +76,7 @@ function clubActivity_01() {
           <div className="shortSum">새로 시작하는 한학기</div>
           <div className="subtitle">노는걸 잘해야 공부도 잘한다</div>
           <div className="detail">모든 국민은 법 앞에 평등하다. 누구든지 성별·종교 또는 사회적 신분에 의하여 정치적·경제적·사회적·문화적 생활의 모든 영역에 있어서 차별을 받지 아니한다.</div>
-          <div className="button"><button><a href="#">동아리 사진첩 바로가기</a></button></div>
+          <div className="button"><button><a href="#!">동아리 사진첩 바로가기</a></button></div>
         </div>
         <div className="image">
           <img src="KakaoTalk_Moim_3j5Aj3GPJCSf5xatggd2eVZXl3R62m.jpg" alt="사진"></img>
@@ -98,7 +101,7 @@ function clubActivity_01() {
             <div className="shortSum">새로 시작하는 한학기</div>
             <div className="subtitle">노는걸 잘해야 공부도 잘한다</div>
             <div className="detail">모든 국민은 법 앞에 평등하다. 누구든지 성별·종교 또는 사회적 신분에 의하여 정치적·경제적·사회적·문화적 생활의 모든 영역에 있어서 차별을 받지 아니한다.</div>
-            <div className="button"><button><a href="#">동아리 사진첩 바로가기</a></button></div>
+            <div className="button"><button><a href="#!">동아리 사진첩 바로가기</a></button></div>
           </div>
 
         </div>
