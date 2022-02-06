d92bf52f5f375484eae4b260be0fdba029465321
지현 <spy03128@naver.com>
2022-02-06 20:53:12 +0900

    fixed footer layout

--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,30 +1,29 @@
-import Activity from './Activity'
-import Board from './Board'
-import Schedule from './Schedule'
-import ScheduleDetaile from './ScheduleDetaile'
-import Footer from '../../components/Footer'
-import Navigation from '../../components/Navigation'
-import ClubIntroduction from './ClubIntroduction'
-import ClubActivities from './ClubActivities'
-import ClubAwards from './ClubAwards'
-import './MainPage.scss'
+import Activity from "./Activity";
+import Board from "./Board";
+import Schedule from "./Schedule";
+import ScheduleDetaile from "./ScheduleDetaile";
+import Footer from "../../components/Footer";
+import Navigation from "../../components/Navigation";
+import ClubIntroduction from "./ClubIntroduction";
+import ClubActivities from "./ClubActivities";
+import ClubAwards from "./ClubAwards";
+import "./MainPage.scss";
 
 function MainPage() {
-    return(
-        <div className='MainPage'>
-            <div className='container'>
-            <Navigation />
-            <Board />
-            <Activity />
-            <Schedule />
-            <ClubIntroduction />
-            <ClubActivities />
-            <ScheduleDetaile />
-            <ClubAwards />
-            <Footer />
-            </div>
-        </div>
-    )
+  return (
+    <div className="MainPage">
+      <div className="container">
+        <Navigation />
+        <Board />
+        <Activity />
+        <Schedule />
+        <ClubIntroduction />
+        <ClubActivities />
+        <ScheduleDetaile />
+        <ClubAwards />
+      </div>
+    </div>
+  );
 }
 
 export default MainPage;
