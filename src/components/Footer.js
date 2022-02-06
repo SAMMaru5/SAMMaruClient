180340d88d7903d025f8186269fd274dc91ddb57
지현 <spy03128@naver.com>
2022-02-06 22:00:37 +0900

    Insert Footer Content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,20 @@
-import './Footer.scss'
+import "./Footer.scss";
 
 function Footer() {
-    return (
-        <div className="Footer">
-            <h1>Footer</h1>
-        </div>
+  return (
+    <div className="Footer">
+      <div className="FooterLeft">
+        <h3>SAMMaru</h3>
+        <hr />
+        <p> Front-End 정원재 박지현 조희진</p>
+        <p> Back-End 조광식 신치용</p>
+      </div>
 
-    )
+      <div className="FooterRight">
+        <h7>dfd</h7>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
