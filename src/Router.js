b184870a3407db097336260164771d6e5a663ade
WonJae Jung <63838039+Lucas0105@users.noreply.github.com>
2022-03-07 15:01:47 +0900

    add login frame (#8)

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation";
 import MainPage from "./pages/main/MainPage";
 import Footer from "./components/Footer";
 import ManagepentPage from "./pages/management/ManagementPage"
+import LoginPage from "./pages/login/LoginPage"
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import PhotoPage from "./pages/photo/PhotoPage";
@@ -23,6 +24,7 @@ function App() {
           <Route path="/notice" element={<NoticePage />}/>
 
           <Route path="/management" element={<ManagepentPage />}/>
+          <Route path="/login" element={<LoginPage />}/>
 
         </Routes>
 
