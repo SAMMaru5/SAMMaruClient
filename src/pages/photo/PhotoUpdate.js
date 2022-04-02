62d217a01114e935d5013368b5779454e899718d
지현 <spy03128@naver.com>
2022-04-02 23:57:53 +0900

    Make PhotoContent Form

--- src/pages/photo/PhotoUpdate.js
+++ src/pages/photo/PhotoUpdate.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Form, Button } from "react-bootstrap";
 
 function PhotoUpdate() {
-  return <div className="content">PhotoUpdate</div>;
+  return (
+    <div
+      className="container"
+      style={{
+        paddingLeft: "100px",
+        paddingRight: "100px",
+        marginBottom: "100px",
+      }}
+    >
+      <Form>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>제목</Form.Label>
+          <Form.Control type="email" placeholder="제목을 입력하세요" />
+        </Form.Group>
+        <Form.Group controlId="formFileMultiple" className="mb-3">
+          <Form.Label>사진 선택</Form.Label>
+          <Form.Control type="file" multiple />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>내용</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button variant="dark" size="lg" abled>
+            작성완료
+          </Button>
+          <Button variant="light" size="lg" abled>
+            작성취소
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
 export default PhotoUpdate;
