d7bfb0556eb5fe05eeb4f57686e908527dc3ee18
지현 <spy03128@naver.com>
2022-04-02 23:27:47 +0900

    error 방지

--- src/pages/photo/PhotoPage.js
+++ src/pages/photo/PhotoPage.js
@@ -1,8 +1,14 @@
 import PhotoList from "./PhotoList";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function PhotoPage() {
+  const navigate = useNavigate();
   return (
     <div className="container">
+      <Button variant="primary" onClick={() => navigate("./photoUpdate")}>
+        글쓰기
+      </Button>
       <PhotoList />
     </div>
   );
---
+++ src/pages/photo/PhotoUpdate.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PhotoUpdate() {
+  return <div>PhotoUpdate</div>;
+}
+
+export default PhotoUpdate;
