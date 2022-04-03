a0cbcb1d0fa8201d2aee80b0a91d2ffc44d41cab
지현 <spy03128@naver.com>
2022-04-03 15:40:58 +0900

    Edit Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,8 @@
 import "./Navigation.scss";
 import { useEffect, useState } from "react";
 import { NavDropdown } from "react-bootstrap";
-import { useNavigate, useLocation} from "react-router-dom";
-import { isAuth } from "./../hooks/useAuth"
+import { useNavigate, useLocation } from "react-router-dom";
+import { isAuth } from "./../hooks/useAuth";
 
 function Navigation() {
   const [show1, setShow1] = useState(false);
@@ -10,23 +10,23 @@ function Navigation() {
   const [show3, setShow3] = useState(false);
   const [show4, setShow4] = useState(false);
   const [show5, setShow5] = useState(false);
-
+  const [show6, setShow6] = useState(false);
 
   const navigate = useNavigate();
   const location = useLocation();
 
-useEffect(() => {
-  isAuth();
-
-}, [location])
-
+  useEffect(() => {
+    isAuth();
+  }, [location]);
 
   return (
     <div className="Navigation container">
       <div className="userStatus">
         <p className="attendance">접속자(0명)</p>
 
-        <p className="user" onClick={() => navigate("/login")}>회원가입/로그인</p>
+        <p className="user" onClick={() => navigate("/login")}>
+          회원가입/로그인
+        </p>
       </div>
 
       <nav>
@@ -57,22 +57,9 @@ useEffect(() => {
           title="공지사항"
           className="nav-dropdown"
           onClick={() => navigate("/notice")}
-          onMouseEnter={(e) => {
-            setShow2(!show2);
-          }}
-          onMouseLeave={(e) => {
-            setShow2(false);
-          }}
-          show={show2}
-        >
-          <div className="navItem2">
-            <NavDropdown.Item>Link 1</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.2">Link 2</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">Link 3</NavDropdown.Item>
-          </div>
-        </NavDropdown>
+        ></NavDropdown>
         <NavDropdown
-          title="자유게시판"
+          title="자료실"
           className="nav-dropdown"
           onMouseEnter={(e) => {
             setShow3(!show3);
@@ -90,28 +77,12 @@ useEffect(() => {
           </div>
         </NavDropdown>
         <NavDropdown
-          title="사진첩"
+          title="자유게시판"
           className="nav-dropdown"
-          onMouseEnter={(e) => {
-            setShow4(!show4);
-          }}
-          onMouseLeave={(e) => {
-            setShow4(false);
-          }}
-          show={show4}
-        >
-          <div className="navItem4">
-            <NavDropdown.Item  onClick={() => navigate("/photo")}>
-              Link 1
-            </NavDropdown.Item>
-            <NavDropdown.Item>
-              Link 2
-            </NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">Link 3</NavDropdown.Item>
-          </div>
-        </NavDropdown>
+          onClick={() => navigate("/")}
+        ></NavDropdown>
         <NavDropdown
-          title="충북대학교"
+          title="사진첩"
           className="nav-dropdown"
           onMouseEnter={(e) => {
             setShow5(!show5);
@@ -120,16 +91,61 @@ useEffect(() => {
             setShow5(false);
           }}
           show={show5}
+          onClick={() => navigate("/photo")}
         >
-          
-          <div className="navItem5">
-            <NavDropdown.Item target="_blank" href="https://www.chungbuk.ac.kr/">충북대학교</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://eis.cbnu.ac.kr/">종합정보시스템</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://software.cbnu.ac.kr/">소프트웨어학과</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://sw7up.cbnu.ac.kr/home">SW중심사업단</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://cbnu.blackboard.com/">ecampus</NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://dorm.chungbuk.ac.kr/">기숙사</NavDropdown.Item>
-
+          <div className="navItem4">
+            <NavDropdown.Item onClick={() => navigate("/photo")}>
+              Link 1
+            </NavDropdown.Item>
+            <NavDropdown.Item>Link 2</NavDropdown.Item>
+            <NavDropdown.Item eventKey="4.3">Link 3</NavDropdown.Item>
+          </div>
+        </NavDropdown>
+        <NavDropdown
+          title="충북대학교"
+          className="nav-dropdown"
+          onMouseEnter={(e) => {
+            setShow6(!show6);
+          }}
+          onMouseLeave={(e) => {
+            setShow6(false);
+          }}
+          show={show6}
+        >
+          <div className="navItem6">
+            <NavDropdown.Item
+              target="_blank"
+              href="https://www.chungbuk.ac.kr/"
+            >
+              충북대학교
+            </NavDropdown.Item>
+            <NavDropdown.Item target="_blank" href="https://eis.cbnu.ac.kr/">
+              종합정보시스템
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              target="_blank"
+              href="https://software.cbnu.ac.kr/"
+            >
+              소프트웨어학과
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              target="_blank"
+              href="https://sw7up.cbnu.ac.kr/home"
+            >
+              SW중심사업단
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              target="_blank"
+              href="https://cbnu.blackboard.com/"
+            >
+              ecampus
+            </NavDropdown.Item>
+            <NavDropdown.Item
+              target="_blank"
+              href="https://dorm.chungbuk.ac.kr/"
+            >
+              기숙사
+            </NavDropdown.Item>
           </div>
         </NavDropdown>
       </nav>
