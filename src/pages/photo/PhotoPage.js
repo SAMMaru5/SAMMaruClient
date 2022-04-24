73c8cf51648a2a7ea375284e3885f89add2fd0b1
지현 <spy03128@naver.com>
2022-04-24 15:24:34 +0900

    fix non-member upload access

--- src/pages/photo/PhotoPage.js
+++ src/pages/photo/PhotoPage.js
@@ -1,12 +1,62 @@
+import React, { useState, useEffect } from "react";
+
 import PhotoList from "./PhotoList";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { getCookie } from "../../hooks/useCookie";
+import Swal from "sweetalert2";
 
 function PhotoPage() {
   const navigate = useNavigate();
+  const [authorizationValue, setAuthorizationValue] = useState("");
+  const [refreshTokenValue, setRefreshTokenValue] = useState("");
+
+  useEffect(() => {
+    const accessToken = getCookie("accessToken");
+    const refreshToken = getCookie("refreshToken");
+    if (accessToken && accessToken !== null) {
+      setAuthorizationValue("Bearer " + accessToken);
+    }
+    if (refreshToken && refreshToken !== null) {
+      setRefreshTokenValue("Bearer " + refreshToken);
+    }
+  }, []);
+
+  const photoUpload = () => {
+    if (authorizationValue == "") {
+      Swal.fire({
+        icon: "error",
+        title: "로그인이 필요합니다.",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+      return;
+    }
+
+    if (refreshTokenValue == "") {
+      Swal.fire({
+        icon: "error",
+        title: "로그인이 필요합니다.",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+      return;
+    }
+    navigate("./photoUpdate");
+  };
+
   return (
     <div className="container">
-      <Button variant="primary" onClick={() => navigate("./photoUpdate")}>
+      <Button
+        variant="primary"
+        onClick={() => {
+          photoUpload();
+        }}
+      >
         글쓰기
       </Button>
       <PhotoList />
