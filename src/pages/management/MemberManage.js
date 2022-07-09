57417a5f4819fa445a4cac12247f50116a3d093a
Lucas0105 <dnjswo105@gmail.com>
2022-07-09 16:45:20 +0900

    feat : /api/users/{userId}/role api 를 사용하여 회원 권한 변경 (#29)

--- src/pages/management/MemberManage.js
+++ src/pages/management/MemberManage.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { call } from "../../hooks/useFetch";
 import "./MemberManage.scss";
+import Swal from "sweetalert2";
 
 function MemberManage(props) {
     const [members, setMembers] = useState({});
@@ -9,7 +10,7 @@ function MemberManage(props) {
     
     useEffect(() => {
 
-        call("/api/users/info/", "GET", "")
+        call("/api/users/info", "GET", "")
         .then((response) => {
             console.log(response);
             if(response.success){
@@ -18,15 +19,54 @@ function MemberManage(props) {
             }
   
         })
-    //   call("/api/user/info", "GET","")
-    //   .then((response) =>{
-    //     console.log(response)
-    //   })
   
     }, [])
 
-    const changeAuthority = (authority)=>{
-        console.log(authority);
+    const changeAuthority = (id, name, authority)=>{
+        let authorityStr = "미지정으"
+        if(authority ==="ROLE_ADMIN"){
+            authorityStr = "관리자"
+        }
+        else if(authority ==="ROLE_MEMBER"){
+            authorityStr = "회원으"
+        }
+
+        Swal.fire({
+            icon: "info",
+            title: `${name} 사용자의 권한을 ${authorityStr}로 바꾸시겠습니까? `,
+            showDenyButton: true,
+            confirmButtonText: '네',
+            denyButtonText: `아니요`
+      
+          }).then((result) => {
+            if (result.isConfirmed) {
+                call("/api/users/"+id+"/role", "PATCH", {"role":authority})
+  
+                    .then((response) => {
+                        console.log(response)
+                        if(response.success){
+                            Swal.fire({
+                                icon: 'success',
+                                title: '권한을 변경하였습니다.',
+                              })
+                        }
+                        else{
+                            Swal.fire({
+                                icon: 'error',
+                                title: '권한 변경에 실패하셨습니다.',
+                              }).then((response)=>{
+                                if(response.isConfirmed){
+                                    window.location.replace("/")
+                                }
+                              })
+                        }
+            
+                    })
+            }
+            else{
+
+            }
+          });
     }
 
     return(
@@ -75,7 +115,7 @@ function MemberManage(props) {
                             </td>
                             <td>
                             {member.role==="ROLE_TEMP" ?
-                                <select defaultValue={"ROLE_TEMP"} onChange={(res)=>{changeAuthority(res.target.value)}}>
+                                <select defaultValue={"ROLE_TEMP"} onChange={(res)=>{changeAuthority(member.userId, member.username, res.target.value)}}>
 
                                     <option value="ROLE_TEMP">
                                         미지정
@@ -88,7 +128,7 @@ function MemberManage(props) {
                                     </option>
                                 </select>
                                 : member.role==="ROLE_MEMBER" ?
-                                    <select defaultValue={"ROLE_MEMBER"}>
+                                    <select defaultValue={"ROLE_MEMBER"}  onChange={(res)=>{changeAuthority(member.userId, member.username, res.target.value)}}>
 
                                         <option value="ROLE_TEMP">
                                             미지정
@@ -101,7 +141,7 @@ function MemberManage(props) {
                                         </option>
                                     </select>
                                 :
-                                    <select defaultValue={"ROLE_ADMIN"}>
+                                    <select defaultValue={"ROLE_ADMIN"}  onChange={(res)=>{changeAuthority(member.userId, member.username, res.target.value)}}>
 
                                         <option value="ROLE_TEMP">
                                             미지정
