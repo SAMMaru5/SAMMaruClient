72c344952343b4fb587695bce8522ef79ed06969
지현 <spy03128@naver.com>
2022-08-07 03:36:55 +0900

    [fix]: 사용안하는 navbar 탭 삭제, navbar 탭 연결 링크 수정 (#75)

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -46,14 +46,14 @@ function Navigation() {
               ) : null}
               <a href="/checkPw">정보 수정</a> |{" "}
               <a
-                href="#!"
+                href="/"
                 onClick={() => {
                   signout();
                 }}
               >
                 로그아웃
               </a>{" "}
-              | <a href="#!">{userInfo.response.username}</a>
+              | <a href="/">{userInfo.response.username}</a>
             </p>
           </div>
         ) : (
@@ -75,7 +75,7 @@ function Navigation() {
         <ul>
           <li>
             <a
-              href="#!"
+              href="/clubIntro"
               className="icon fa-chart-bar"
               onMouseEnter={(e) => {
                 setShow1(!show1);
@@ -91,9 +91,7 @@ function Navigation() {
               <li>
                 <a href="/clubIntro">동아리 소개</a>
               </li>
-              <li>
-                <a href="#!">활동</a>
-              </li>
+
               <li>
                 <a href="/awards">수상 경력</a>
               </li>
@@ -140,12 +138,12 @@ function Navigation() {
             </a>
           </li>
           <li>
-            <a href="#!" className="icon fa-chart-bar">
+            <a href="/seminar" className="icon fa-chart-bar">
               <span>자료실</span>
             </a>
             <ul>
               <li>
-                <a href="#!">특강자료</a>
+                <a href="/seminar">특강자료</a>
               </li>
               <li>
                 <a href="/report">활동보고서</a>
@@ -169,7 +167,10 @@ function Navigation() {
             </a>
           </li>
           <li>
-            <a className="icon solid fa-sitemap" href="no-sidebar.html">
+            <a
+              className="icon solid fa-sitemap"
+              href="https://www.chungbuk.ac.kr/"
+            >
               <span>충북대학교</span>
             </a>
             <ul>
@@ -231,112 +232,7 @@ function Navigation() {
           </li>
         </ul>
       </nav>
-      {/* 
-      <nav>
-        <img
-          src="img/logo.png"
-          alt="로고 이미지"
-          style={{ cursor: "pointer" }}
-          onClick={() => navigate("/")}
-        />
-        <NavDropdown
-          title="샘마루"
-          className="nav-dropdown"
-          onMouseEnter={(e) => {
-            setShow1(!show1);
-          }}
-          onMouseLeave={(e) => {
-            setShow1(false);
-          }}
-          show={show1}
-        >
-          <div className="navItem1">
-            <NavDropdown.Item onClick="4.1">Link 1</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.2">Link 2</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">Link 3</NavDropdown.Item>
-          </div>
-        </NavDropdown>
-        <NavDropdown
-          title="공지사항"
-          className="nav-dropdown"
-          onClick={() => navigate("/notice")}
-        ></NavDropdown>
-        <NavDropdown
-          title="자료실"
-          className="nav-dropdown"
-          onMouseEnter={(e) => {
-            setShow3(!show3);
-          }}
-          onMouseLeave={(e) => {
-            setShow3(false);
-          }}
-          show={show3}
-        >
-          <div className="navItem3">
-            <NavDropdown.Item>특강 자료</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.2">활동보고서</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">소규모프젝</NavDropdown.Item>
-            <NavDropdown.Item eventKey="4.3">족보</NavDropdown.Item>
-          </div>
-        </NavDropdown>
-        <NavDropdown
-          title="자유게시판"
-          className="nav-dropdown"
-          onClick={() => navigate("/freeBoard")}
-        ></NavDropdown>
-        <NavDropdown
-          title="사진첩"
-          className="nav-dropdown"
-          onClick={() => navigate("/photo")}
-        ></NavDropdown>
-        <NavDropdown
-          title="충북대학교"
-          className="nav-dropdown"
-          onMouseEnter={(e) => {
-            setShow6(!show6);
-          }}
-          onMouseLeave={(e) => {
-            setShow6(false);
-          }}
-          show={show6}
-        >
-          <div className="navItem6">
-            <NavDropdown.Item
-              target="_blank"
-              href="https://www.chungbuk.ac.kr/"
-            >
-              충북대학교
-            </NavDropdown.Item>
-            <NavDropdown.Item target="_blank" href="https://eis.cbnu.ac.kr/">
-              종합정보시스템
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              target="_blank"
-              href="https://software.cbnu.ac.kr/"
-            >
-              소프트웨어학과
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              target="_blank"
-              href="https://sw7up.cbnu.ac.kr/home"
-            >
-              SW중심사업단
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              target="_blank"
-              href="https://cbnu.blackboard.com/"
-            >
-              ecampus
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              target="_blank"
-              href="https://dorm.chungbuk.ac.kr/"
-            >
-              기숙사
-            </NavDropdown.Item>
-          </div>
-        </NavDropdown>
-      </nav> */}
+
       <HelmetProvider>
         <Helmet>
           <script src="/assets/js/jquery.min.js"></script>
