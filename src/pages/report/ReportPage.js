bf05fcf64e714ded7199da9a860f0de1dde51906
developersung13 <developersung13@gmail.com>
2022-09-18 21:49:14 +0900

    [fix]: 활동 보고서 게시판 배너 이미지 비율 유지 스타일 적용 (#126)

--- src/pages/report/ReportPage.js
+++ src/pages/report/ReportPage.js
@@ -147,7 +147,8 @@ function ReportPage() {
         <img
           src={report}
           alt="활동보고서 배너"
-          style={{ width: "100%", height: "200px" }}
+          // height값을 auto로 변경하여 브라우저의 크기가 변경되어도 이미지 비율 유지
+          style={{ width: "100%", height: "auto" }}
         ></img>
         <div className="location">
           <img className="home" src="home.png" alt="home"></img>
