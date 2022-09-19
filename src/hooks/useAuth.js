ca787c74abe05b9b1880f94e5b5df51ee770d7bd
developersung13 <developersung13@gmail.com>
2022-09-19 18:12:13 +0900

    [fix]: Prettier & ESLint 포맷팅 적용 (#128)

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,77 +1,81 @@
 import { call } from "./useFetch";
-import { setCookie, delCookie, getCookie} from './useCookie';
-import jwtDecode from 'jwt-decode'
+import { setCookie, delCookie, getCookie } from "./useCookie";
+import jwtDecode from "jwt-decode";
 
 //refreshToken 갱신
-export function isAuth(){
-    const accessToken = getCookie("accessToken");
-    const refreshToken = getCookie("refreshToken");
+export function isAuth() {
+  const accessToken = getCookie("accessToken");
+  const refreshToken = getCookie("refreshToken");
 
-    if((accessToken && accessToken !== null) || (refreshToken && refreshToken !== null))
-        call("/auth/reissue", "POST", "")
-        .then((response) => {
-            const todayDate = new Date();
-            try{
-                if(response.success){
-                    const decode = jwtDecode (response.response.accessToken);
-                    setCookie("accessToken", response.response.accessToken, decode.exp - todayDate.getTime()/1000);
+  if (
+    (accessToken && accessToken !== null) ||
+    (refreshToken && refreshToken !== null)
+  )
+    call("/auth/reissue", "POST", "").then((response) => {
+      const todayDate = new Date();
+      try {
+        if (response.success) {
+          const decode = jwtDecode(response.response.accessToken);
+          setCookie(
+            "accessToken",
+            response.response.accessToken,
+            decode.exp - todayDate.getTime() / 1000
+          );
 
-                    const decode2 = jwtDecode (response.response.refreshToken);
-                    setCookie("refreshToken", response.response.refreshToken, decode2.exp - todayDate.getTime()/1000);
-                    return response
-                    // window.location.href = "/";
-                }
-                else{
-                    return false;
-                }
-            }
-            catch(err){
-                return console.log(err)
-            }
-        })
+          const decode2 = jwtDecode(response.response.refreshToken);
+          setCookie(
+            "refreshToken",
+            response.response.refreshToken,
+            decode2.exp - todayDate.getTime() / 1000
+          );
+          return response;
+          // window.location.href = "/";
+        } else {
+          return false;
+        }
+      } catch (err) {
+        return console.log(err);
+      }
+    });
 }
 
-export function login(SignInRequest){
-    return call("/auth/login", "POST", SignInRequest).then((response)=>{
-        const todayDate = new Date();
-        try{
-            
-            if(response.response.accessToken){
-                const decode = jwtDecode (response.response.accessToken);
-                setCookie("accessToken", response.response.accessToken, decode.exp - todayDate.getTime()/1000);
-                return response
-                // window.location.href = "/";
-            }
-        }
-        catch{
-            return false
-        }
-        
+export function login(SignInRequest) {
+  return call("/auth/login", "POST", SignInRequest).then((response) => {
+    const todayDate = new Date();
+    try {
+      if (response.response.accessToken) {
+        const decode = jwtDecode(response.response.accessToken);
+        setCookie(
+          "accessToken",
+          response.response.accessToken,
+          decode.exp - todayDate.getTime() / 1000
+        );
+        return response;
+        // window.location.href = "/";
+      }
+    } catch {
+      return false;
     }
-    );
+  });
 }
 
-export function myRole(){
-
-    return call("/no-permit/api/user/info", "GET", '').then((response)=>{
-        if(response.success === false){
-            return "not authorized"
-        }
-        else if(response.response.role ==="ROLE_TEMP"){
-            return "temp"
-        }
-        else if(response.response.role === "ROLE_MEMBER") {
-            return "member"
-        }
-        else if(response.response.role === "ROLE_ADMIN") {
-            return "admin"
-        }
-    })
+export function myRole() {
+  return call("/no-permit/api/user/info", "GET", "").then((response) => {
+    if (response.success === false) {
+      return "not authorized";
+    } else if (response.response.role === "ROLE_TEMP") {
+      return "temp";
+    } else if (response.response.role === "ROLE_MEMBER") {
+      return "member";
+    } else if (response.response.role === "ROLE_ADMIN") {
+      return "admin";
+    }
+  });
 }
 
 export function signout() {
-    delCookie("accessToken")
-    delCookie("refreshToken")
+  delCookie("accessToken");
+  delCookie("refreshToken");
 
-    window.location.href = "/";
-  }
+  window.location.href = "/";
+}
