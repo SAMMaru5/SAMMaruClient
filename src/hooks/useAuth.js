b19905b93fbd18334ca51b1a139bb838200bef3b
developersung13 <developersung13@gmail.com>
2022-09-25 17:56:52 +0900

    [bug]: 로그아웃 함수 내 불필요 코드 제거 (#168)

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,37 +1,23 @@
 import api from "../utils/api";
 import Swal from "sweetalert2";
-
-
+import { delCookie, getCookie } from "./useCookie";
 
 export function myRole() {
-    return api.get("/no-permit/api/user/info").then((response)=>{
-        if (!response) {
-            return "not authorized";
-        } else if (response.data.response.role === "ROLE_TEMP") {
-            return "temp";
-        } else if (response.data.response.role === "ROLE_MEMBER") {
-            return "member";
-        } else if (response.data.response.role === "ROLE_ADMIN") {
-            return "admin";
-        }
-    });
+  return api.get("/no-permit/api/user/info").then((response) => {
+    if (!response) {
+      return "not authorized";
+    } else if (response.data.response.role === "ROLE_TEMP") {
+      return "temp";
+    } else if (response.data.response.role === "ROLE_MEMBER") {
+      return "member";
+    } else if (response.data.response.role === "ROLE_ADMIN") {
+      return "admin";
+    }
+  });
 }
 
 export function signout() {
-    api.delete("/auth/logout").then(response => {
-        sessionStorage.removeItem("EXPIRED_TIME");
-        if(response.data.success){
-            Swal.fire({
-                icon: 'success',
-                title: '로그아웃에 성공하셨습니다.',
-            });
-        }
-        else{
-            Swal.fire({
-                icon: 'error',
-                title: '로그아웃에 실패하셨습니다.',
-            });
-        }
-    })
+  api.delete("/auth/logout").then((response) => {
     window.location.href = "/";
+  });
 }
