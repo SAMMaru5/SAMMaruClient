17cd8b13543449d832853fe3926930f6038afeec
developersung13 <developersung13@gmail.com>
2022-09-27 23:29:14 +0900

    [bug]: 게시판 생성 컴포넌트 내 액세스 토큰 유효 여부 확인 코드 추가 (#170)

--- src/pages/management/BoardManage.js
+++ src/pages/management/BoardManage.js
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
 import Swal from "sweetalert2";
 import api from "../../utils/api";
-import { myRole } from "../../hooks/useAuth";
+import { delCookie, getCookie } from "../../hooks/useCookie";
 
 function BoardManage() {
   const [board, setBoard] = useState({ boardName: "", description: "" });
@@ -12,42 +12,51 @@ function BoardManage() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    function expiredLogin() {
+      Swal.fire({
+        title: "로그인 상태 허용 시간이 초과되었습니다.",
+        text: "로그인 페이지로 다시 이동하시겠습니까?",
+        icon: "error",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "확인",
+        cancelButtonText: "취소",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/login";
+        }
+      });
+    }
 
-    myRole().then((response) => {
-      if (response === "not authorized") {
-        Swal.fire({
-          icon: "error",
-          title: "로그인이 필요합니다.",
-        }).then((result) => {
-          navigate("/login");
+    async function getUserInfo() {
+      try {
+        await api.get("/no-permit/api/user/info").then((response) => {
+          api.post("/api/boards", board).then((response) => {
+            if (response.data.success) {
+              Swal.fire({
+                icon: "success",
+                title: "게시판 생성을 성공했습니다.",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  navigate("/");
+                }
+              });
+            } else {
+              Swal.fire({
+                icon: "error",
+                title: "게시판 생성을 실패했습니다.",
+              });
+            }
+          });
         });
-      } else if (response !== "admin") {
-        Swal.fire({
-          icon: "info",
-          title: "접근 권한이 없습니다. 관리자에게 문의해 주세요.",
-        }).then((result) => {
-          navigate("/login");
-        });
+      } catch (error) {
+        delCookie("SammaruAccessToken");
+        expiredLogin();
       }
-    });
-
-    api.post("/api/boards", board).then((response) => {
-      if (response.data.success) {
-        Swal.fire({
-          icon: "success",
-          title: "게시판 생성을 성공했습니다.",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            navigate("/");
-          }
-        });
-      } else {
-        Swal.fire({
-          icon: "error",
-          title: "게시판 생성을 실패했습니다.",
-        });
-      }
-    });
+    }
+    if (getCookie("SammaruAccessToken")) getUserInfo();
+    else expiredLogin();
   };
 
   return (
