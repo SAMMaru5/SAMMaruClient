1136467124980f897d67791cc5b73c978b770aa3
developersung13 <developersung13@gmail.com>
2022-10-02 18:22:42 +0900

    [feat]: 게시글 관리 컴포넌트 및 게시글 삭제 API 호출 모듈 추가 (#182)

---
+++ src/hooks/usePostServices.js
@@ -0,0 +1,30 @@
+import api from "../utils/api";
+import Swal from "sweetalert2";
+import { createBrowserHistory } from "history";
+import { checkExpiredAccesstoken } from "./useAuth";
+
+export const deletePost = (props) => {
+  const customHistory = createBrowserHistory();
+  checkExpiredAccesstoken().then((response) => {
+    if (response) {
+      try {
+        api
+          .delete(`/api/boards/${props.boardId}/articles/${props.articleId}`)
+          .then((response) => {
+            Swal.fire({
+              icon: "success",
+              title: "게시글을 삭제하였습니다.",
+            }).then((result) => {
+              customHistory.back();
+            });
+          });
+      } catch (error) {
+        console.log(error);
+        return Swal.fire({
+          icon: "error",
+          title: "게시글이 삭제 중 오류가 발생하였습니다.",
+        });
+      }
+    }
+  });
+};
