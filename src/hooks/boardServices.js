a410e420464ce14bb04f1be657cd6aebfa567acd
developersung13 <developersung13@gmail.com>
2022-12-15 20:04:00 +0900

    [feat]: 게시글 검색 기능 수행 함수 추가 (#201)

--- src/hooks/boardServices.js
+++ src/hooks/boardServices.js
@@ -1,6 +1,5 @@
 import api from "../utils/api";
 import Swal from "sweetalert2";
-import { delCookie } from "./useCookie";
 
 export const getBoardList = async () => {
   try {
@@ -34,5 +33,31 @@ export const getBoards = async (location) => {
     );
   } catch (error) {
     window.location.href = "/login";
+  }
+};
+
+/**
+ * 게시판 내 (검색 구분) 선택 및 (키워드) 입력 후
+ * 검색 버튼 클릭 시 '특정 게시글 검색' 기능을 수행하는 함수
+ * @param {number} boardId 게시판 id값을 넣어주세요
+ * @param {number} pageNum 게시판 내 페이지 번호를 넣어주세요
+ * @param {string} keyword 검색 문자열 키워드를 넣어주세요
+ * @param {string} keywordSelectOption 검색 구분으로 사용될 문자열 값을 넣어주세요
+ */
+export const searchArticles = async (
+  boardId,
+  pageNum,
+  keyword,
+  keywordSelectOption
+) => {
+  try {
+    return await api.get(
+      `/no-permit/api/boards/${boardId}/searchResult/pages/${pageNum}?keyword=${keyword}&pageSize=${10}&searchSubject=${keywordSelectOption}`
+    );
+  } catch (error) {
+    Swal.fire({
+      icon: "info",
+      title: "게시판에 등록된 글이 없습니다.",
+    });
   }
 };
