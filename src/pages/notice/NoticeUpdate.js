e2885826f1e10785069b3767cbed686f26d7eb6e
developersung13 <developersung13@gmail.com>
2022-12-28 11:18:20 +0900

    [bug]: 공지사항 게시글 작성 컴포넌트 내 'unique key' warning 제거 (#243)

--- src/pages/notice/NoticeUpdate.js
+++ src/pages/notice/NoticeUpdate.js
@@ -172,7 +172,10 @@ function NoticeRegisteration() {
                     <Card>
                       <ListGroup variant="flush">
                         {showFiles.map((file, id) => (
-                          <ListGroup.Item className="d-flex justify-content-between align-items-center">
+                          <ListGroup.Item
+                            className="d-flex justify-content-between align-items-center"
+                            key={id}
+                          >
                             <span>
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
