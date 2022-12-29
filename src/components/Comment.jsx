06675c5beb20ba4384d20182719e1b66c7da9a43
YEOLAM <56868605+developersung13@users.noreply.github.com>
2022-12-29 17:04:50 +0900

    [fix]: 게시글 댓글 '수정 & 삭제' 기능 사용의 대상 설정 로직 수정 (#247) (#248)
    * [fix]: 게시글 댓글 '수정 & 삭제' 기능 사용의 대상 설정 로직 수정 (#247)
    
    * [fix]: 미사용 메소드 import문 제거 (#247)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,6 @@ import api from "../utils/api";
 import "./Comment.scss";
 import Swal from "sweetalert2";
 import { checkExpiredAccesstoken, myRole } from "../hooks/useAuth";
-import { delCookie, getCookie } from "../hooks/useCookie";
 
 function Comment(props) {
   const [page, setPage] = useState([]);
@@ -21,7 +20,7 @@ function Comment(props) {
       try {
         await api.get("/no-permit/api/user/info").then((response) => {
           if (response.data.success) {
-            setUserInfo(response.data);
+            setUserInfo(response.data.response);
           } else {
             setUserInfo(null);
           }
@@ -30,7 +29,7 @@ function Comment(props) {
         localStorage.clear();
       }
     }
-    localStorage.getItem('sm-accessToken') && getUserInfo();
+    localStorage.getItem("sm-accessToken") && getUserInfo();
     myRole().then((response) => {
       setRole(response);
     });
@@ -245,7 +244,7 @@ function Comment(props) {
                     </div>
                     <p className="content">{comment.content}</p>
                     <div className="commentControl">
-                      {userInfo.response.username === comments[i].author ? (
+                      {userInfo.userId === comments[i].userId ? (
                         <>
                           <button
                             className="modifyingComment"
@@ -258,7 +257,7 @@ function Comment(props) {
                         </>
                       ) : null}
                       {role === "admin" ||
-                      userInfo.response.username === comment.author ? (
+                      userInfo.userId === comment.userId ? (
                         <button
                           className="deletingComment"
                           onClick={() => deleteComment(comment.id)}
@@ -267,7 +266,6 @@ function Comment(props) {
                         </button>
                       ) : null}
                     </div>
-                    {/* <span>디엠 수정</span> */}
                   </td>
                 </tr>
               ))
