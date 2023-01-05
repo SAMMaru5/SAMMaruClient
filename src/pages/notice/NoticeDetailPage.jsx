96bf822450701756b7019a3eb202a2ee10ca9510
developersung13 <developersung13@gmail.com>
2023-01-06 00:20:28 +0900

    [feat]: 공지사항 게시글 뒤로가기 시 이전 pagination값 전달 (#251)

--- src/pages/notice/NoticeDetailPage.jsx
+++ src/pages/notice/NoticeDetailPage.jsx
@@ -5,20 +5,43 @@ import { getBoards } from "../../hooks/boardServices";
 import Comment from "../../components/Comment";
 import { deletePost } from "../../hooks/usePostServices";
 import { UploadedFilesInArticle } from "../../components/UploadedFilesInArticle";
+import { createBrowserHistory } from "history";
 
 function NoticeDetailPage() {
   const navigate = useNavigate();
   const location = useLocation();
+  const history = createBrowserHistory();
+
   const [article, setArticle] = useState({});
   const [loading, setLoading] = useState(false);
   const [like, setLike] = useState(false);
+  const [pageNum, setPageNum] = useState(1);
+  const [locationKeys, setLocationKeys] = useState([]);
 
   useEffect(() => {
     getBoards(location).then((data) => {
+      setPageNum(location.state.pageNum);
       setArticle(data.data.response);
       setLoading(true);
     });
   }, [location]);
+
+  // 페이지 '뒤로가기'를 감지하는 코드
+  useEffect(() => {
+    return history.listen((location) => {
+      if (history.action === "PUSH") {
+        setLocationKeys([location.key]);
+      }
+
+      if (history.action === "POP") {
+        navigate("/notice", {
+          state: {
+            pageNum,
+          },
+        });
+      }
+    });
+  }, [locationKeys, history]);
 
   return (
     <div className="NoticeDetail">
@@ -89,7 +112,11 @@ function NoticeDetailPage() {
             <button
               className="grey darken-3"
               onClick={() => {
-                navigate("/notice");
+                navigate("/notice", {
+                  state: {
+                    pageNum,
+                  },
+                });
               }}
             >
               목록
