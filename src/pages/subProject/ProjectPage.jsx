03f3dcf2285734ee934fa15aba2b6fb3f73b712b
developersung13 <developersung13@gmail.com>
2023-01-06 00:09:24 +0900

    [feat]: 소규모 프로젝트 게시판 내 pagination 값 prop 속성 사용 및 전달 코드 추가 (#251)

--- src/pages/subProject/ProjectPage.jsx
+++ src/pages/subProject/ProjectPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Pagination } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import project from "../../imgs/banner/project.jpg";
 import { myRole } from "../../hooks/useAuth";
 import {
@@ -9,12 +9,13 @@ import {
   searchArticles,
 } from "../../hooks/boardServices";
 
-function ProjectPage() {
+function ProjectPage(props) {
   const navigate = useNavigate();
+  const location = useLocation();
   const [boardList, setBoardList] = useState({});
   const [loading, setLoading] = useState(false);
   const [boardId, setBoardId] = useState();
-  const [pageNum, setPageNum] = useState(1);
+  const [pageNum, setPageNum] = useState(props.pageNum);
   const [pageList, setPageList] = useState(1);
   const [keywordSelectOption, setKeywordSelectOption] =
     useState("ARTICLE_TITLE");
@@ -23,7 +24,7 @@ function ProjectPage() {
   useEffect(() => {
     // 비회원의 권한 확인
     myRole().then((response) => {
-      if (response != undefined) {
+      if (response !== undefined) {
         getBoardList().then((response) => {
           if (response.data.success) {
             response.data.response.forEach((res) => {
@@ -45,6 +46,10 @@ function ProjectPage() {
     else setPageList(parseInt(pageNum / 10) * 10 + 1);
   }, [pageNum]);
 
+  useEffect(() => {
+    location.state && setPageNum(location.state.pageNum);
+  }, [location]);
+
   const examUpload = () => {
     navigate("./projectUpdate");
   };
@@ -54,6 +59,7 @@ function ProjectPage() {
       state: {
         boardId,
         articleId: list.id,
+        pageNum,
       },
     });
   };
