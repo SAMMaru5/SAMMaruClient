aefc9296dc8e2d15f822f3e3d26560f1e184f65b
YEOLAM <56868605+developersung13@users.noreply.github.com>
2023-02-10 20:57:41 +0900

    [bug]: 만료된 JWT 토큰을 이용한 API 사용으로 인해 발생하는 버그 제거 (#274) (#275)

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { signout } from "../hooks/useAuth";
 
 const instance = axios.create({
   withCredentials: true,
@@ -6,7 +7,7 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(async function (config) {
-  const currentTime = new Date();
+  const currentTime = new Date().getTime();
   const accessToken = localStorage.getItem("sm-accessToken");
   const refreshToken = localStorage.getItem("sm-refreshToken");
 
@@ -17,20 +18,36 @@ instance.interceptors.request.use(async function (config) {
 
   if (localStorage.getItem("sm-expired")) {
     const expiredTime = new Date(
-        localStorage.getItem("sm-expired").toString().substring(0, 19)
-    );
+      localStorage.getItem("sm-expired").toString().substring(0, 19)
+    ).getTime();
 
-    if (expiredTime.getTime() - currentTime.getTime() <= 300000) {
+    // 만료 시간 이후에 API의 호출이 일어난 경우 로컬 스토리지 초기화
+    if (expiredTime < currentTime) {
+      localStorage.clear();
+      return config;
+    }
+
+    // 만료 시간 2분 내에 API의 호출이 일어나면 리이슈 요청
+    if (expiredTime - currentTime <= 120000) {
       await axios
-        .post(
-          process.env.REACT_APP_URL + "/auth/reissue",
-          {accessToken, refreshToken}
-        )
+        .post(process.env.REACT_APP_URL + "/auth/reissue", {
+          accessToken,
+          refreshToken,
+        })
         .then((res) => {
-          if(res.status === 200){
-            localStorage.setItem('sm-accessToken', res.data.response.accessToken);
-            localStorage.setItem('sm-refreshToken', res.data.response.refreshToken);
-            localStorage.setItem('sm-expired', res.data.response.accessTokenExpiresTime);
+          if (res.status === 200) {
+            localStorage.setItem(
+              "sm-accessToken",
+              res.data.response.accessToken
+            );
+            localStorage.setItem(
+              "sm-refreshToken",
+              res.data.response.refreshToken
+            );
+            localStorage.setItem(
+              "sm-expired",
+              res.data.response.accessTokenExpiresTime
+            );
           }
         });
     }
@@ -39,11 +56,8 @@ instance.interceptors.request.use(async function (config) {
   return config;
 });
 
-instance.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-);
-
+instance.interceptors.response.use((response) => {
+  return response;
+});
 
 export default instance;
