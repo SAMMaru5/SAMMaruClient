99d248e38e57ec973c9b0346a8d114ef28287232
developersung13 <developersung13@gmail.com>
2023-02-15 21:51:18 +0900

    [fix]: JWT 인증 방식 변경에 따른 interceptor 코드 수정 (#278)

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,9 +7,8 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(async function (config) {
-  const currentTime = new Date().getTime();
   const accessToken = localStorage.getItem("sm-accessToken");
-  const refreshToken = localStorage.getItem("sm-refreshToken");
+  const currentTime = new Date().getTime();
 
   config.headers = {
     Authorization: `Bearer ${accessToken}`,
@@ -30,23 +29,20 @@ instance.interceptors.request.use(async function (config) {
     // 만료 시간 2분 내에 API의 호출이 일어나면 리이슈 요청
     if (expiredTime - currentTime <= 120000) {
       await axios
-        .post(process.env.REACT_APP_URL + "/auth/reissue", {
-          accessToken,
-          refreshToken,
-        })
-        .then((res) => {
-          if (res.status === 200) {
+        .post(
+          process.env.REACT_APP_URL + "/auth/reissue",
+          { accessToken },
+          { withCredentials: true }
+        )
+        .then((response) => {
+          if (response.status === 200) {
             localStorage.setItem(
               "sm-accessToken",
-              res.data.response.accessToken
-            );
-            localStorage.setItem(
-              "sm-refreshToken",
-              res.data.response.refreshToken
+              response.data.response.accessToken
             );
             localStorage.setItem(
               "sm-expired",
-              res.data.response.accessTokenExpiresTime
+              response.data.response.accessTokenExpiresTime
             );
           }
         })
